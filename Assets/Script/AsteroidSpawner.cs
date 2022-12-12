ead5ed23e77709360971496d5089df0ff2790109
NoahMarkowitz <noahb.markowitz@gmail.com>
2022-12-12 18:08:40 -0500

    try commit

---
+++ Assets/Script/AsteroidSpawner.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+
+    public PowerUp powerupPrefab;
+
+    // How often an steroid will spawn
+    public float spawnRate = 10f;
+
+}
